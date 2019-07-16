97728f6754adfcb781b3336e10860c3ee6a4f065
David Araujo <david.araujo91@gmail.com>
2019-07-16 10:19:11 -0300

    Get requested url parsing it and loging the request path.

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // Dependencies - All ready available on Node
 const http = require('http')
+const url = require('url') // Module to work easily with urls, making parsings an other cool stuffs.
 
 // Create a server that will reponse all requests with "Hello World"
 const server = http.createServer((req, res) => {
+    
+    // Get request url and parse it.
+    const parsedUrl = url.parse(req.url, true)
+
+    // Get the path
+    const path = parsedUrl.path
+    const trimmedPath = path.replace(/^\/+|\/+$/, '')
+
+    // Send the response
     res.end('Hello World!')
+
+    // Log the request path
+    console.log(trimmedPath)
 })
 
 // Start the Server, listen on Port 3000
-server.listen(3000, () => console.log('Server is listening on Port: 3000') )
+server.listen( 3000, () => console.log('Server is listening on Port: 3000') )
